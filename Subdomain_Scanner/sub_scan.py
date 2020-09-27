f41a817562d90b50b4a92f81cf78f38b649a02bb
Nhien.IT <44127534+nhienit2010@users.noreply.github.com>
2020-09-27 20:39:48 +0700

    Subdomain_scanner

---
+++ Subdomain_Scanner/sub_scan.py
@@ -0,0 +1,39 @@
+import requests, getopt, sys
+
+def Usage():
+	print("Usage:")
+	print("\tpython3 sub_scan.py -h <host>")
+	print("Options:")
+	print("\t--help: For help")
+	return
+
+with open('subdomains-top1million-110000.txt', 'r') as f:
+	wordlists = [x.strip() for x in f.read().split('\n')]
+
+def scan(host):
+	for word in wordlists:
+		try:
+			url = 'https://'+ word + '.' + host
+			r =	requests.get(url)
+			if r.status_code >= 200 and r.status_code <= 403:
+				print(url)
+		except:
+			pass
+	return
+
+if __name__ == '__main__':
+	try:
+		if (len(sys.argv) < 2):
+			Usage()
+			sys.exit(2)
+		
+		opts, args = getopt.getopt(sys.argv[1:], "h:", ["help"])
+
+		for opt, arg in opts:
+			if opt == "--help":
+				Usage()
+				sys.exit(2)
+			if opt == "-h":
+				scan(arg)
+	except:
+		print("Error")
